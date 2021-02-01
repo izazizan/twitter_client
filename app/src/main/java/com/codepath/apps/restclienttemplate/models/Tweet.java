24d14dcdc77125ff6554ef9b25c1c66ecf609456
izazizan <faizadraws@gmail.com>
2021-02-01 13:16:26 -0800

    Added ComposeTweet Activity

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -5,16 +5,19 @@ import com.codepath.apps.restclienttemplate.TimeFormatter;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Parcel
 public class Tweet {
     public String body;
     public String relDate;
     public String createdAt;
     public User user;
     public long id;
+    // empty constructor needed by the Parceler library
+    public Tweet(){}
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException
     {
